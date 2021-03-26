9202e610fc08ba0c0496d386665810bcf9996da0
Arrose <arrose.chen@foxmail.com>
2021-03-26 08:22:19 +0800

    init

---
+++ ComputerScienceFoundation/cmake-build-debug/CMakeFiles/ComputerScienceFoundation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ComputerScienceFoundation.dir/main.cpp.obj"
+  "ComputerScienceFoundation.exe"
+  "ComputerScienceFoundation.exe.manifest"
+  "ComputerScienceFoundation.pdb"
+  "libComputerScienceFoundation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ComputerScienceFoundation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
